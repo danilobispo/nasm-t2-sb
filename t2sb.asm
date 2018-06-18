d73616016446555e34269e2c7ba94ce684c4ff8a
HAL-9000 <danilobispogalvao@gmail.com>
2018-06-18 19:44:21 -0300

    Removido o pulo de linha no input do usuário, deixando mensagem mais legível

--- t2sb.asm
+++ t2sb.asm
@@ -1,9 +1,9 @@
 section .data                           ;Data segment
    userMsg db 'Informe seu nome de usuário: ' ;Ask the user to enter a number
    lenUserMsg equ $-userMsg             ;The length of the message
-   dispMsg db 'Hóla, '
+   dispMsg db 'Hóla,'
    lenDispMsg equ $-dispMsg
-   dispMsg2 db ',bem vindo ao programa de CALC IA-32'               
+   dispMsg2 db ' ,bem vindo ao programa de CALC IA-32'               
    lenDispMsg2 equ $-dispMsg2
 
 section .bss           ;Uninitialized data
@@ -12,7 +12,7 @@ section .bss           ;Uninitialized data
 section .text          ;Code Segment
    global _start
 	
-_start:                ;User prompt
+_start:                ; Mostra mensagem para usuário informar o nome
    mov eax, 4
    mov ebx, 1
    mov ecx, userMsg
@@ -23,17 +23,23 @@ _start:                ;User prompt
    mov eax, 3
    mov ebx, 2
    mov ecx, num  
-   mov edx, 100          ;5 bytes (numeric, 1 for sign) of that information
+   mov edx, 100          ;100 bytes é o tamanho máximo do nome(100 caracteres)
    int 80h
+
+trata_string:
+   dec eax
+   cmp byte[ecx+eax], 0xa ; Verifica se o último caractere é newline
+   je remove_char
 	
-   ;Output the message 'The entered number is: '
+mostra_mensagem:
+   ;Mostra a mensagem 'Hóla, '
    mov eax, 4
    mov ebx, 1
    mov ecx, dispMsg
    mov edx, lenDispMsg
    int 80h  
 
-   ;Output the number entered
+   ;Por fim, exibe o nome inserido pelo usuário
    mov eax, 4
    mov ebx, 1
    mov ecx, num
@@ -51,3 +57,7 @@ _start:                ;User prompt
    mov eax, 1
    mov ebx, 0
    int 80h
+
+remove_char:
+   mov byte[ecx+eax], 0
+   jmp mostra_mensagem
